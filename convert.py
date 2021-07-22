3ebf066628a486f2532a3f7f1e6357b3466b70f4
konsprin <kossarpl@gmail.com>
2021-07-22 13:39:05 +0200

    loading graphs from Stanford Large Network Dataset Collection

---
+++ convert.py
@@ -0,0 +1,34 @@
+# %%
+import os 
+from igraph import *
+
+file_name = "p2p-Gnutella08.txt"
+# file_name = "web-Google.txt"
+
+file_name_absolute = os.getcwd() + "\graphs\\" + file_name
+
+def load_graph(file):
+  edges = []
+
+  lines = f.readlines()
+
+  nnodes = int(lines[2].split(" ")[2])
+  for line in lines[4:]:
+    edges.append([int(s.strip()) for s in line.split("\t")])
+  return nnodes, edges
+
+#  %%
+try:
+  with open(file_name, "r") as f:
+    nnodes, edges = load_graph(f)
+except: 
+  try: 
+    with open(file_name_absolute, "r") as f:
+      nnodes, edges = load_graph(f)
+  except:
+    pass
+
+print(edges[-4:])
+
+g = Graph(n=nnodes, edges=edges, directed=True)
+g.save(file_name.split(".")[0] + ".graphml")
